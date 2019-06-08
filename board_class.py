bc1ed0e1bbff18b1a50d5677e494195c7209d9ad
xsist14 <jason.d.guest@gmail.com>
2019-06-08 13:59:18 -0700

    Created class files for pieces and board, created board on main file and added gitignore

---
+++ board_class.py
@@ -0,0 +1,1 @@
+# a board consists of 64 squares
---
+++ main.py
@@ -0,0 +1,10 @@
+
+
+print "[r][h][b][q][k][b][h][r]"
+print "[p][p][p][p][p][p][p][p]"
+print "[x][x][x][x][x][x][x][x]"
+print "[x][x][x][x][x][x][x][x]"
+print "[x][x][x][x][x][x][x][x]"
+print "[x][x][x][x][x][x][x][x]"
+print "[p][p][p][p][p][p][p][p]"
+print "[r][h][b][q][k][b][h][r]"
